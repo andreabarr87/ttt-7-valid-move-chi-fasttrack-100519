678e23ede66409ab28c1e3c1a60ecd54ab9286dc
Andrea S Barr <andreabarr87@gmail.com>
2019-10-06 17:33:37 +0000

    Automatically backed up by Learn

--- lib/valid_move.rb
+++ lib/valid_move.rb
@@ -1,8 +1,6 @@
-
-
 def valid_move?(board, index)
   if (board[index] >= 0) && (board[index] <= 8) 
-    || if position_taken(board, index) == false
+    || if position_taken?(board, index) == false
   end
   return true
 end
